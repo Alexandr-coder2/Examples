ae68568dd43d797afdf849ef8787f0bf55e5c224
Alexandr-coder2 <120954936+Alexandr-coder2@users.noreply.github.com>
2023-01-26 17:15:10 +0500

    добавили проект Маша

---
+++ examples/example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользавателя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.Write("Ура,это же МАША! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
